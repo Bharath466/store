52af10b3ee39769da5cb65356c3ddc80f5953916
Bharath <bharathbandi835@gmail.com>
2023-08-19 22:15:09 +0530

    Added models

--- reliance/admin.py
+++ reliance/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from . import models
+admin.site.register(models.Customer)
+admin.site.register(models.Product)
+admin.site.register(models.Order)
