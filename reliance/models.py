5baab95147da491b8a6de570f4b132e66798e280
Bharath <bharathbandi835@gmail.com>
2023-08-20 16:10:10 +0530

    Till 10 vid

--- reliance/models.py
+++ reliance/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+class Tag(models.Model):
+    name=models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 class Customer(models.Model):
     name=models.CharField(max_length=200)
-    phone=models.CharField(max_length=12)
-    email=models.EmailField()
-    date_created=models.DateTimeField(auto_now_add=True)
+    phone=models.CharField(max_length=12,null=True)
+    email=models.EmailField(null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
         return self.name
@@ -18,10 +23,12 @@ class Product(models.Model):
     price=models.IntegerField(max_length=5)
     category=models.CharField(max_length=10,choices=CATEGORY)
     description=models.CharField(max_length=500)
-    date_created=models.DateTimeField(auto_now_add=True)
-
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    tags=models.ManyToManyField(Tag)
     def __str__(self):
         return self.name
+
+
     
 class Order(models.Model):
     STATUS=(
@@ -29,6 +36,8 @@ class Order(models.Model):
         ('Out for delivery','Out for delivery'),
         ('Delivered','Delivered')
     )
-    date_created=models.DateTimeField(auto_now_add=True)
+    customer=models.ForeignKey(Customer,null=True,on_delete=models.SET_NULL)
+    product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField(max_length=100,choices=STATUS)
-    
+   
--- reliance/urls.py
+++ reliance/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[path('home/',views.home,name='home'),
-             path('customer/',views.customer,name='customer'),
+             path('customer/<str:pk>',views.customer,name='customer'),
              path('products/',views.products,name='products'),
+             path('create_order/',views.createOrder,name='create_order'),
+             path('update_order/<str:pk>/',views.updateOrder,name='update_order')
              ]
